6b8c049ed7cf7fcc5d56c20f91cb191ce0f192e1
Kober <pedrokoberstain2@gmail.com>
2024-02-21 09:26:58 -0400

    feat: Cria o serviço de criar um usuário

--- src/main/java/com/compassuol/challenge3/User/service/UserService.java
+++ src/main/java/com/compassuol/challenge3/User/service/UserService.java
@@ -1,9 +1,12 @@
 package com.compassuol.challenge3.User.service;
 
+import com.compassuol.challenge3.User.model.User;
 import com.compassuol.challenge3.User.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -11,4 +14,7 @@ public class UserService {
     @Autowired
     UserRepository repository;
 
+    public User createUser(User user) {
+        return repository.save(user);
+    }
 }
