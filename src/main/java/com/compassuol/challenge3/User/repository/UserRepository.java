abdb6d40fdb899b985a9073a61f9c93e30d42268
Kober <pedrokoberstain2@gmail.com>
2024-02-22 15:45:48 -0400

    feat: Incluindo o findById no Repository para rodar os testes

--- src/main/java/com/compassuol/challenge3/User/repository/UserRepository.java
+++ src/main/java/com/compassuol/challenge3/User/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.compassuol.challenge3.User.repository;
 
+import com.compassuol.challenge3.User.data.vo.v1.UserVO;
 import com.compassuol.challenge3.User.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
 }
