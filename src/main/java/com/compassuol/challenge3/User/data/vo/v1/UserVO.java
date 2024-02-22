a04ea845cbe0ad52b749b26a3e0d3a15e2b83162
Kober <pedrokoberstain2@gmail.com>
2024-02-22 13:44:14 -0400

    feat: Adicionando os diretorios e criando o UserVO

---
+++ src/main/java/com/compassuol/challenge3/User/data/vo/v1/UserVO.java
@@ -0,0 +1,17 @@
+package com.compassuol.challenge3.User.data.vo.v1;
+
+import java.io.Serializable;
+
+public class UserVO implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private String birthdate;
+    private String email;
+    private String cep;
+    private String password;
+    private boolean active;
+
+}
