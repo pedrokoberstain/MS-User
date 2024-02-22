d24323316f1dc325dea5d8ac0694dc67ff4dd373
Kober <pedrokoberstain2@gmail.com>
2024-02-22 15:45:13 -0400

    feat: Pequenas mudanças no Dozer mapper para adquação ao projeto

--- src/main/java/com/compassuol/challenge3/User/mapper/DozerMapper.java
+++ src/main/java/com/compassuol/challenge3/User/mapper/DozerMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class DozerMapper {
 
-    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
@@ -16,7 +16,7 @@ public class DozerMapper {
 
     public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
         List<D> destinationObjects = new ArrayList<D>();
-        for (O o : origin) {
+        for (Object o : origin) {
             destinationObjects.add(mapper.map(o, destination));
         }
         return destinationObjects;
