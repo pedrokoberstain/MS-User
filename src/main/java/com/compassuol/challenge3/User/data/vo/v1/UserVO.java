0434cfb8d5d0347085c629b4b642002a8a7ea9f1
Kober <pedrokoberstain2@gmail.com>
2024-02-22 15:47:45 -0400

    feat: Adicionando Getter e Setter no UserVO - Criado o MockUser para os testes -Criado os ConverterTest

--- src/main/java/com/compassuol/challenge3/User/data/vo/v1/UserVO.java
+++ src/main/java/com/compassuol/challenge3/User/data/vo/v1/UserVO.java
@@ -1,7 +1,16 @@
 package com.compassuol.challenge3.User.data.vo.v1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVO implements Serializable {
 
     private Long id;
--- src/test/java/com/compassuol/challenge3/User/unittests/mapper/DozerConverterTest.java
+++ src/test/java/com/compassuol/challenge3/User/unittests/mapper/DozerConverterTest.java
@@ -1,4 +1,69 @@
 package com.compassuol.challenge3.User.unittests.mapper;
 
+import com.compassuol.challenge3.User.data.vo.v1.UserVO;
+import com.compassuol.challenge3.User.mapper.DozerMapper;
+import com.compassuol.challenge3.User.model.User;
+import com.compassuol.challenge3.User.unittests.mapper.mocks.MockUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DozerConverterTest {
+
+    private MockUser mockUser;
+
+    @BeforeEach
+    public void setUp() {
+        mockUser = new MockUser();
+    }
+
+    @Test
+    public void testParseEntityToVO() {
+        User entity = mockUser.mockEntity(0);
+        UserVO vo = DozerMapper.parseObject(entity, UserVO.class);
+        assertUserEquals(entity, vo);
+    }
+
+    @Test
+    public void testParseEntityListToVOList() {
+        List<User> entityList = mockUser.mockEntityList();
+        List<UserVO> voList = DozerMapper.parseListObjects(entityList, UserVO.class);
+        for (int i = 0; i < entityList.size(); i++) {
+            assertEquals(entityList.get(i).getId(), voList.get(i).getId());
+        }
+    }
+
+    @Test
+    public void testParseVOListToEntityList() {
+        List<UserVO> voList = mockUser.mockVOList();
+        List<User> entityList = DozerMapper.parseListObjects(voList, User.class);
+        for (int i = 0; i < voList.size(); i++) {
+            assertEquals(voList.get(i).getId(), entityList.get(i).getId());
+        }
+    }
+
+    private void assertUserEquals(User expected, UserVO actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getFirstName(), actual.getFirstName());
+        assertEquals(expected.getLastName(), actual.getLastName());
+        assertEquals(expected.getEmail(), actual.getEmail());
+        assertEquals(expected.getCpf(), actual.getCpf());
+        assertEquals(expected.getCep(), actual.getCep());
+        assertEquals(expected.getBirthdate(), actual.getBirthdate());
+        assertEquals(expected.isActive(), actual.isActive());
+    }
+
+    private void assertUserVOEquals(UserVO expected, User actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getFirstName(), actual.getFirstName());
+        assertEquals(expected.getLastName(), actual.getLastName());
+        assertEquals(expected.getEmail(), actual.getEmail());
+        assertEquals(expected.getCpf(), actual.getCpf());
+        assertEquals(expected.getCep(), actual.getCep());
+        assertEquals(expected.getBirthdate(), actual.getBirthdate());
+        assertEquals(expected.isActive(), actual.isActive());
+    }
 }
