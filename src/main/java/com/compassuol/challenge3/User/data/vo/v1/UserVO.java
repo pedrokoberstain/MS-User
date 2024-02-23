d9c52200aecb02c65334f1761aec68916ea8d4a4
Kober <pedrokoberstain2@gmail.com>
2024-02-22 23:08:19 -0400

    feat: Adicionando as validações no nome, email e password

--- src/main/java/com/compassuol/challenge3/User/data/vo/v1/UserVO.java
+++ src/main/java/com/compassuol/challenge3/User/data/vo/v1/UserVO.java
@@ -1,5 +1,7 @@
 package com.compassuol.challenge3.User.data.vo.v1;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +15,19 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class UserVO implements Serializable {
 
+
     private Long id;
+    @NotBlank(message = "The first name must be at least 3 characters long")
     private String firstName;
     private String lastName;
     private String cpf;
+
     private String birthdate;
+    @NotBlank
+    @Email(message = "Invalid email address", regexp = "^[A-Za-z0-9+_.-]+@(.+)$")
     private String email;
     private String cep;
+    @NotBlank(message = "The password must be at least 6 characters long")
     private String password;
     private boolean active;
 
