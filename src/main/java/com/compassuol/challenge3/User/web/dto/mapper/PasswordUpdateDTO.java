f94cc516205566fea0822433e0ae903818946fab
Kober <pedrokoberstain2@gmail.com>
2024-02-22 23:28:49 -0400

    feat: Criando os DTOs necessários

---
+++ src/main/java/com/compassuol/challenge3/User/web/dto/mapper/PasswordUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.compassuol.challenge3.User.web.dto.mapper;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateDTO implements Serializable {
+    @NotBlank(message = "The password must be at least 6 characters long")
+    private String password;
+}
---
+++ src/main/java/com/compassuol/challenge3/User/web/dto/mapper/UserUpdateDTO.java
@@ -0,0 +1,23 @@
+package com.compassuol.challenge3.User.web.dto.mapper;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDTO implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private String birthdate;
+    @Email(message = "Invalid email address", regexp = "^[A-Za-z0-9+_.-]+@(.+)$")
+    private String email;
+    private String cep;
+}
