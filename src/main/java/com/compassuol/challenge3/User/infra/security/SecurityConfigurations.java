9ea7996742d2519817fa7d9a5b188634e10c26be
Kober <pedrokoberstain2@gmail.com>
2024-02-25 03:53:40 -0400

    feat: adicionando a validação de autenticação do usuario

--- src/main/java/com/compassuol/challenge3/User/infra/security/SecurityConfigurations.java
+++ src/main/java/com/compassuol/challenge3/User/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.compassuol.challenge3.User.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,9 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(crsf -> crsf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .build();
     }
 }
