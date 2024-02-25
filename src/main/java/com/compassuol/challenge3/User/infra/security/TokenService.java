c48db92417bce3949f2baedf7e105b103aa89936
Kober <pedrokoberstain2@gmail.com>
2024-02-25 14:53:30 -0400

    feat: Criando o serviço para validar o Token.

--- src/main/java/com/compassuol/challenge3/User/infra/security/TokenService.java
+++ src/main/java/com/compassuol/challenge3/User/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.compassuol.challenge3.User.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.compassuol.challenge3.User.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,19 @@ public class TokenService {
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error creating token");
 
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            return "";
         }
     }
 
