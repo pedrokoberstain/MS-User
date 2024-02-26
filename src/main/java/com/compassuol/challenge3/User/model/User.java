3ffd1a9b01705661d44eaca8f144a2237a38a144
Kober <pedrokoberstain2@gmail.com>
2024-02-25 20:31:50 -0400

    fix: Correção dos endpoints para funcionamento do jwt

--- src/main/java/com/compassuol/challenge3/User/model/User.java
+++ src/main/java/com/compassuol/challenge3/User/model/User.java
@@ -2,17 +2,23 @@ package com.compassuol.challenge3.User.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.*;
-import org.hibernate.validator.constraints.br.CPF;
-import org.springframework.format.annotation.DateTimeFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -20,62 +26,50 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
-    @Column(name = "first_name", nullable = false)
-    @Size(min = 3, max = 80, message = "The first name must be at least 3 characters long")
+    @NotBlank(message = "O primeiro nome é obrigatório e deve ter no mínimo 3 caracteres")
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
-    @Size(min = 3, max = 80, message = "The last name must be at least 3 characters long")
+    @NotBlank(message = "O sobrenome é obrigatório e deve ter no mínimo 3 caracteres")
     private String lastName;
 
-    @CPF(message = "Invalid CPF")
-    @Column(unique = true, nullable = false)
+    @NotBlank(message = "O CPF é obrigatório")
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "Formato de CPF inválido")
+    @Column(unique = true)
     private String cpf;
 
-    @NotNull(message = "The birthdate must be provided")
-    @Temporal(TemporalType.DATE)
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @JsonFormat(pattern = "dd/MM/yyyy")
-    private Date birthdate;
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthdate;
 
-    @Email(message = "Invalid email address")
-    @Column(unique = true, nullable = false)
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Formato de email inválido")
+    @Column(unique = true)
     private String email;
 
-    @Pattern(regexp = "^\\d{5}-\\d{3}$", message = "Invalid CEP")
     private String cep;
 
-    @Column(nullable = false)
-    @Size(min = 6, message = "The password must be at least 6 characters long")
+    @NotBlank(message = "A senha é obrigatória e deve ter no mínimo 6 caracteres")
     private String password;
 
-    @NotNull(message = "The user must be active or inactive")
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @ColumnDefault("true")
     private boolean active;
-
-    @AssertTrue(message = "The user must be active or inactive")
-    private boolean isActiveValid() {
-        return active == true || active == false;
-    }
-
-    private UserRole role;
-
-    public User(String email, String password, UserRole role) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority("USER"));
-        else return List.of(new SimpleGrantedAuthority("USER"));
+        if (this.userRole == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
     }
 
     @Override
@@ -100,6 +94,11 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return active;
+        return true;
+    }
+
+    // Método para formatar a data de nascimento para o padrão dd/MM/yyyy ao serializar o objeto
+    public String getBirthdateFormatted() {
+        return birthdate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 }
