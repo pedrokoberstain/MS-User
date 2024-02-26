592e1913c26f52314c501e8ba52471d7af679a85
Kober <pedrokoberstain2@gmail.com>
2024-02-25 20:33:33 -0400

    fix: Atualizações para ser possivel gerar o token de login

--- src/main/java/com/compassuol/challenge3/User/web/dto/RegisterDTO.java
+++ src/main/java/com/compassuol/challenge3/User/web/dto/RegisterDTO.java
@@ -1,18 +1,48 @@
 package com.compassuol.challenge3.User.web.dto;
 
 import com.compassuol.challenge3.User.model.UserRole;
-import jakarta.validation.constraints.*;
-import org.hibernate.validator.constraints.br.CPF;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+public class RegisterDTO {
 
-public record RegisterDTO(
-        @NotBlank(message = "First name is required") String firstName,
-        @NotBlank(message = "Last name is required") String lastName,
-        @CPF(message = "Invalid CPF") String cpf,
-        @NotNull(message = "Birthdate must be provided") Date birthdate,
-        @Email(message = "Invalid email address") String email,
-        @Pattern(regexp = "^\\d{5}-\\d{3}$", message = "Invalid CEP") String cep,
-        @NotBlank(message = "Password is required") String password,
-        @NotNull(message = "Role is required") UserRole role
-) {}
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "CPF is required")
+    private String cpf;
+
+    @NotBlank(message = "Birthdate is required")
+    private String birthdate;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "CEP is required")
+    private String cep;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return UserRole.USER;
+    }
+}
