7653ef5565050a672852e0ac2bb15395821360b4
Kober <pedrokoberstain2@gmail.com>
2024-02-27 11:34:53 -0400

    feat: Implementação dos DTOs Request e Response e ajuste para funcionamento

--- src/main/java/com/compassuol/challenge3/User/repository/UserRepository.java
+++ src/main/java/com/compassuol/challenge3/User/repository/UserRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID>{
+public interface UserRepository extends JpaRepository<User, Long>{
     UserDetails findByEmail(String email);
+
+    Optional<User> findById(Long id);
+
+
 }
