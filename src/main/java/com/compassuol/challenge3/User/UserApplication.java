db9848b46c22a386d6b357878361f7f13467619b
Kober <pedrokoberstain2@gmail.com>
2024-02-29 22:35:17 -0400

    feat: Adicionado Modelo de Protocolo de Solicitação de Notificação e Exceção de Solicitação de Notificação

--- src/main/java/com/compassuol/challenge3/User/UserApplication.java
+++ src/main/java/com/compassuol/challenge3/User/UserApplication.java
@@ -1,13 +1,16 @@
 package com.compassuol.challenge3.User;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/compassuol/challenge3/User/model/ProtocoloSolicitacaoNotification.java
@@ -0,0 +1,10 @@
+package com.compassuol.challenge3.User.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProtocoloSolicitacaoNotification {
+    private String proctocolo;
+}
