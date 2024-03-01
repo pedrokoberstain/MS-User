5dac779f41a2bf9a61644b3fed802d64755484f1
Kober <pedrokoberstain2@gmail.com>
2024-02-29 22:34:10 -0400

    feat: Adicionado Funcionalidade de Notificação no Controlador de Usuário

--- src/main/java/com/compassuol/challenge3/User/web/controller/UserController.java
+++ src/main/java/com/compassuol/challenge3/User/web/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.compassuol.challenge3.User.web.controller;
 
+import com.compassuol.challenge3.User.exception.SolicitacaoNotificationException;
+import com.compassuol.challenge3.User.model.EmissaoNotification;
+import com.compassuol.challenge3.User.model.ProtocoloSolicitacaoNotification;
 import com.compassuol.challenge3.User.model.User;
 import com.compassuol.challenge3.User.service.UserService;
 import com.compassuol.challenge3.User.web.dto.UserResponseDto;
@@ -7,8 +10,11 @@ import com.compassuol.challenge3.User.web.dto.UserSenhaDto;
 import com.compassuol.challenge3.User.web.dto.UserUpdateDto;
 import com.compassuol.challenge3.User.web.dto.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,22 +22,21 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final RabbitTemplate rabbitTemplate;
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
         User user = userService.getById(id);
+        sendNotification(user.getEmail(), "getUserById");
         return ResponseEntity.ok(UserMapper.toDto(user));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserUpdateDto userUpdateDto) {
-        if (userUpdateDto.getCpf() == null || userUpdateDto.getBirthdate() == null) {
-            return ResponseEntity.badRequest().body("CPF e Birthdate são campos obrigatórios");
-        }
-
         User userToUpdate = userService.getById(id);
         User updatedUser = UserMapper.toEntity(userUpdateDto, userToUpdate);
         updatedUser = userService.update(id, updatedUser);
+        sendNotification(updatedUser.getEmail(), "updateUser");
         return ResponseEntity.ok(UserMapper.toDto(updatedUser));
     }
 
@@ -39,8 +44,31 @@ public class UserController {
     @PatchMapping("/{id}/password")
     public ResponseEntity<String> updatePassword(@PathVariable Long id, @RequestBody UserSenhaDto dto) {
         userService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+        sendNotification(userService.getById(id).getEmail(), "updatePassword");
         return ResponseEntity.ok("Senha alterada com sucesso");
     }
 
+    @PostMapping("/notification")
+    public ResponseEntity solicitarEmissao(@RequestBody EmissaoNotification emissaoNotification) {
+        try {
+            ProtocoloSolicitacaoNotification protocolo = (ProtocoloSolicitacaoNotification) userService
+                    .solicitaremissaoNotification(emissaoNotification);
+            return ResponseEntity.ok(protocolo);
+        } catch (SolicitacaoNotificationException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
 
+    private void sendNotification(String email, String event) {
+        try {
+            EmissaoNotification notification = new EmissaoNotification();
+            notification.setEmail(email);
+            notification.setEvent(event);
+            notification.setDate(LocalDateTime.now().toString());
+
+            userService.solicitaremissaoNotification(notification);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
