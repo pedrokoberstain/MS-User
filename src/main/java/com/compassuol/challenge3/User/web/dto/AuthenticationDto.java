e116ce9e93b8ffbac0c6d2d8d8c1d82a5b8343d6
Kober <pedrokoberstain2@gmail.com>
2024-02-29 22:33:47 -0400

    feat: Integração de RabbitMQ no Controlador de Autenticação

--- src/main/java/com/compassuol/challenge3/User/web/dto/AuthenticationDto.java
+++ src/main/java/com/compassuol/challenge3/User/web/dto/AuthenticationDto.java
@@ -2,5 +2,9 @@ package com.compassuol.challenge3.User.web.dto;
 
 public record AuthenticationDto(String email, String password) {
 
+
+    public String getEmail() {
+        return email;
+    }
 }
 
